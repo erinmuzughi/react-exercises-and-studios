96365048317993a0699b2393fb109c8fa7021325
erinmuzughi <erinmuzughi@gmail.com>
2023-08-03 17:56:36 -0500

    Completed Part 1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "My Three Favorite Book Series";
+   let book1 = "https://target.scene7.com/is/image/Target/GUEST_6d0a4da5-9997-4727-b04c-9789d7321b1f?wid=1000&hei=1000&fit=constrain&qlt=80&fmt=webp";
+   let book2 = "https://m.media-amazon.com/images/I/81jI3PJ8b1L.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/51YZRhokbnL._SY498_BO1,204,203,200_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="A Courth of Thorns and Roses Series" />
+         <img src={book2} alt="Throne of Glass Series" />
+         <img src={book3} alt="Cresent City Series" />
       </div>      
    );
 }
